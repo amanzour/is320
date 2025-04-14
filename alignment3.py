7ab9f9f3ab88050a75e178fe509859f31ee3589a
BrentF-1 <bfilks1@gmail.com>
2025-04-13 23:37:56 -0400

    Update alignment3.py
    Random game I had on my computer that I made, I do not know if it works.

--- alignment3.py
+++ alignment3.py
@@ -1,1 +1,38 @@
+from random import random
+import turtle as tri
+import random as rand
 
+
+    
+    
+
+#-----game configuration----
+
+# sh = shape, c = color, fc = fillcolor
+
+sh = "triangle"
+c = "pink"
+fc = "pink"
+tri.shapesize(1)
+tri.shape(sh)
+tri.fillcolor(fc)
+tri.setheading(90)
+ 
+def onclick(): 
+ ()
+if (tri.onclick):
+ x2 = rand.randint (-250,250) 
+ y2 = rand.randint (-300, 300)
+ def change_posit():
+  (x2,y2)
+ 
+def tri_clicked(a,b):
+ change_posit
+ tri.penup()
+ tri.goto(x2,y2)
+ tri.pendown()
+
+tri.onclick(tri_clicked)
+
+wn = tri.Screen()
+wn.mainloop()
