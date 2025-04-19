4fd3a69cdd0dc5433d0a022a27fb24f1997afb47
William Castellino <castelw@sunypoly.edu>
2025-04-19 12:16:01 -0400

    Added Scrumply

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ more lines
 
 This is a description about this repository. IS 320 is a System Analysis and Design class
 edited by Group 2 :P
+
+Any unauthorized personel will be personally visited by Scrumply, who will NOT be happy.
--- alignment3.py
+++ alignment3.py
@@ -1,1 +1,14 @@
-
+# > "Added Scrumply"
+# > looks inside
+# > eldritch horror????
+print("Scrumply shows mercy to no man, woman, or child.")
+print("You will never be far enough to evade him.")
+print("You will never be strong enough to fight him.")
+print("You will never be clever enough to outwit him.")
+print("You will never be prepared enough to meet him.")
+print("When the wraith awakes, you will not be safe.")
+print("You will never be safe.")
+print("You will never survive.")
+print("Prayer won't work, for he is the one you should be praying to.")
+print("None of your transgressions will be forgiven.")
+print("The pain will feel like eternity.")
